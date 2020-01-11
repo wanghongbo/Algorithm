d37b79798d2935613e21eb12c46b5a32eac270ea
WHB <wanghongboskate@hotmail.com>
2020-01-12 02:12:07 +0800

    查找两个有序数组的中位数

--- Algorithm/LeetCode/4.FindMedianSortedArrays.cs
+++ Algorithm/LeetCode/4.FindMedianSortedArrays.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+
 /*
 There are two sorted arrays nums1 and nums2 of size m and n respectively.
 
@@ -22,7 +23,64 @@ The median is (2 + 3)/2 = 2.5
 
 namespace Algorithm.LeetCode {
     public class FindMedianSortedArrays {
-        public static double Perform(int[] nums1, int[] nums2) {
+        /// <summary>
+        /// 方法1，时间复杂度O(m + n)
+        /// </summary>
+        /// <param name="nums1"></param>
+        /// <param name="nums2"></param>
+        /// <returns></returns>
+        public static double Perform1(int[] nums1, int[] nums2) {
+            if (nums1.Length > nums2.Length) {
+                return Perform1(nums2, nums1);
+            }
+            return BinarySearch(nums1, 0, nums1.Length, nums2);
+        }
+
+        private static double BinarySearch(int[] nums1, int left, int right, int[] nums2) {
+            int m = nums1.Length;
+            int n = nums2.Length;
+            int i = (right - left + 1) / 2 + left;
+            int j = (m + n + 1) / 2 - i;
+            if (i < m && nums1[i] < nums2[j - 1]) {
+                return BinarySearch(nums1, i, right, nums2);    //向右查找
+            } else if (i > 0 && nums1[i - 1] > nums2[j]) {
+                return BinarySearch(nums1, left, i - 1, nums2); //向左查找
+            } else {
+                if ((m + n) % 2 == 0) {
+                    return (MaxLeft(nums1, i - 1, nums2, j - 1) + MinRight(nums1, i, nums2, j) ) / 2.0;
+                } else {
+                    return MaxLeft(nums1, i - 1, nums2, j - 1);
+                }
+            }
+        }
+
+        private static int MaxLeft(int[] nums1, int i, int[] nums2, int j) {
+            if (i < 0) {
+                return nums2[j];
+            } else if (j < 0) {
+                return nums1[i];
+            } else {
+                return Math.Max(nums1[i], nums2[j]);
+            }
+        }
+
+        private static int MinRight(int[] nums1, int i, int[] nums2, int j) {
+            if (i >= nums1.Length) {
+                return nums2[j];
+            } else if (j >= nums2.Length) {
+                return nums1[i];
+            } else {
+                return Math.Min(nums1[i], nums2[j]);
+            }
+        }
+
+        /// <summary>
+        /// 方法2，时间复杂度为O((m + n) / 2)，达不到O(lg(m + n))
+        /// </summary>
+        /// <param name="nums1"></param>
+        /// <param name="nums2"></param>
+        /// <returns></returns>
+        public static double Perform2(int[] nums1, int[] nums2) {
             int length = (nums1.Length + nums2.Length) / 2 + 1;
             int[] aux = new int[length];
             int i = 0;
--- UnitTest/LeetCode测试/4.FindMedianSortedArraysTester.cs
+++ UnitTest/LeetCode测试/4.FindMedianSortedArraysTester.cs
@@ -10,7 +10,7 @@ namespace UnitTest.LeetCode测试 {
         public void TestCase1() {
             int[] nums1 = { 1, 2 };
             int[] nums2 = { 3, 4 };
-            double result = FindMedianSortedArrays.Perform(nums1, nums2);
+            double result = Perform(nums1, nums2);
             Assert.AreEqual(result, 2.5);
         }
 
@@ -18,7 +18,7 @@ namespace UnitTest.LeetCode测试 {
         public void TestCase2() {
             int[] nums1 = { 1, 3 };
             int[] nums2 = { 2 };
-            double result = FindMedianSortedArrays.Perform(nums1, nums2);
+            double result = Perform(nums1, nums2);
             Assert.AreEqual(result, 2.0);
         }
 
@@ -26,8 +26,84 @@ namespace UnitTest.LeetCode测试 {
         public void TestCase3() {
             int[] nums1 = { 1, 3, 4, 7 };
             int[] nums2 = { 2 };
-            double result = FindMedianSortedArrays.Perform(nums1, nums2);
+            double result = Perform(nums1, nums2);
             Assert.AreEqual(result, 3.0);
+        }
+
+        [TestMethod]
+        public void TestCase4() {
+            int[] nums1 = { 1, 3, 4, 7 };
+            int[] nums2 = { 0 };
+            double result = Perform(nums1, nums2);
+            Assert.AreEqual(result, 3.0);
+        }
+
+        [TestMethod]
+        public void TestCase5() {
+            int[] nums1 = { 1, 3, 4, 7 };
+            int[] nums2 = { 8 };
+            double result = Perform(nums1, nums2);
+            Assert.AreEqual(result, 4.0);
+        }
+
+        [TestMethod]
+        public void TestCase6() {
+            int[] nums1 = { 1, 3, 4, 7 };
+            int[] nums2 = { };
+            double result = Perform(nums1, nums2);
+            Assert.AreEqual(result, 3.5);
+        }
+
+        [TestMethod]
+        public void TestCase7() {
+            int[] nums1 = { 1, 2 };
+            int[] nums2 = { 3, 4, 5, 6, 7, 8 };
+            double result = Perform(nums1, nums2);
+            Assert.AreEqual(result, 4.5);
+        }
+
+        [TestMethod]
+        public void TestCase8() {
+            int[] nums1 = { 1, 2 };
+            int[] nums2 = { 3, 4, 5, 6, 7 };
+            double result = Perform(nums1, nums2);
+            Assert.AreEqual(result, 4.0);
+        }
+
+        [TestMethod]
+        public void TestCase9() {
+            int[] nums1 = { 9, 10 };
+            int[] nums2 = { 3, 4, 5, 6, 7, 8 };
+            double result = Perform(nums1, nums2);
+            Assert.AreEqual(result, 6.5);
+        }
+
+        [TestMethod]
+        public void TestCase10() {
+            int[] nums1 = { 9, 10 };
+            int[] nums2 = { 3, 4, 5, 6, 7 };
+            double result = Perform(nums1, nums2);
+            Assert.AreEqual(result, 6.0);
+        }
+
+        [TestMethod]
+        public void TestCase11() {
+            int[] nums1 = { 3 };
+            int[] nums2 = { -2, -1 };
+            double result = Perform(nums1, nums2);
+            Assert.AreEqual(result, -1.0);
+        }
+
+        [TestMethod]
+        public void TestCase12() {
+            int[] nums1 = { 1, 1, 3, 3 };
+            int[] nums2 = { 1, 1, 3, 3 };
+            double result = Perform(nums1, nums2);
+            Assert.AreEqual(result, 2.0);
+        }
+
+        public double Perform(int[] nums1, int[] nums2) {
+            return FindMedianSortedArrays.Perform1(nums1, nums2);
         }
     }
 }
