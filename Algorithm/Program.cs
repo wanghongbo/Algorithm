100678b63fcc60c7d861b9ee86c660e57d6e9905
WHB <wanghongboskate@hotmail.com>
2020-06-27 17:23:50 +0800

    顺时针打印矩阵

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -2,38 +2,7 @@
 namespace Algorithm {
     class Program {
         static void Main(string[] args) {
-            string str = "a";
-            string pattern = ".*";
-            bool result = Perform(str, pattern);
-            System.Console.WriteLine(result);
-        }
 
-        public static bool Perform(string str, string pattern) {
-            return Mach(str, 0, pattern, 0);
-        }
-
-        static bool Mach(string str, int i, string pattern, int j) {
-            if (i == str.Length && j == pattern.Length) {
-                return true;
-            }
-            if (i == str.Length && j != pattern.Length) {
-                return false;
-            }
-            if (i != str.Length && j == pattern.Length) {
-                return false;
-            }
-            if (j + 1 < pattern.Length && pattern[j + 1] == '*') {
-                if (str[i] == pattern[j] || pattern[j] == '.') {
-                    return Mach(str, i, pattern, j + 2) || Mach(str, i + 1, pattern, j + 2) || Mach(str, i + 1, pattern, j);
-                } else {
-                    return Mach(str, i, pattern, j + 2);
-                }
-            } else {
-                if (str[i] == pattern[j] || pattern[j] == '.') {
-                    return Mach(str, i + 1, pattern, j + 1);
-                }
-                return false;
-            }
         }
     }
 }
---
+++ Algorithm/剑指Offer/29.顺时针打印矩阵.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.剑指Offer {
+    class PrintCircle {
+        public static void Perform(int[,] arr2d) {
+            printCircleCore(arr2d, 0, 0, arr2d.GetLength(0) - 1, arr2d.GetLength(1) - 1);
+        }
+
+        static void printCircleCore(int[,] arr2d, int xMin, int yMin, int xMax, int yMax) {
+            while (xMin <= xMax && yMin <= yMax) {
+                for (int y = yMin; y <= yMax; y++) {
+                    System.Console.WriteLine(arr2d[xMin, y]);
+                }
+                for (int x = xMin + 1; x <= xMax; x++) {
+                    System.Console.WriteLine(arr2d[x, yMax]);
+                }
+                for (int y = yMax - 1; y >= yMin; y--) {
+                    System.Console.WriteLine(arr2d[xMax, y]);
+                }
+                for (int x = xMax - 1; x >= xMin + 1; x--) {
+                    System.Console.WriteLine(arr2d[x, yMin]);
+                }
+                xMin += 1;
+                yMin += 1;
+                xMax -= 1;
+                yMax -= 1;
+            }
+        }
+    }
+}
