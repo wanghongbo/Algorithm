d683adaab0553e6fdc32b442b20c0d5b69170391
WHB <wanghongboskate@hotmail.com>
2020-06-27 18:33:26 +0800

    二叉搜索数与双向链表

---
+++ Algorithm/剑指Offer/36.二叉搜索数与双向链表.cs
@@ -0,0 +1,41 @@
+﻿
+namespace Algorithm.剑指Offer {
+    class ConvertTwoWayLinkedList {
+        public static Component Perform(Node head) {
+            if (head == null) return null;
+            Component n = new Component();
+            Component componentLeft = Perform(head.left);
+            Component componentRight = Perform(head.right);
+            if (componentLeft != null) {
+                n.front = componentLeft.front;
+                componentLeft.end.left = head;
+                head.right = componentLeft.end;
+            } else {
+                n.front = head;
+            }
+            if (componentRight != null) {
+                n.end = componentRight.end;
+                componentRight.front.right = head;
+                head.left = componentRight.front;
+            } else {
+                n.end = head;
+            }
+            return n;
+        }
+    }
+
+    class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int v) {
+            this.value = v;
+        }
+    }
+
+    class Component {
+        public Node front;
+        public Node end;
+    }
+}
