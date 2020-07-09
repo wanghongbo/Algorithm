31fc63caf071d2533cb0031617c3fc70027dbba5
WHB <wanghongboskate@hotmail.com>
2020-07-09 22:53:02 +0800

    字符串的排列

---
+++ Algorithm/剑指Offer/38.字符串的排列.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Algorithm.剑指Offer {
+    public class Permutation {
+
+        public static void Perform(char[] str, int start) {
+            if (start == str.Length) {
+                System.Console.WriteLine(str);
+                return;
+            }
+            for (int k = start; k < str.Length; k++) {
+                char temp = str[start];
+                str[start] = str[k];
+                str[k] = temp;
+
+                Perform(str, start + 1);
+
+                temp = str[start];
+                str[start] = str[k];
+                str[k] = temp;
+            }
+        }
+
+        static void Test() {
+            char[] str = "abcd".ToCharArray();
+            Permutation.Perform(str, 0);
+        }
+    }
+}
