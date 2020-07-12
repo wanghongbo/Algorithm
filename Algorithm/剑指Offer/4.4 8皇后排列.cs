411afe6bd28f73fcf39a001c7ed651dc4ae6fde2
WHB <wanghongboskate@hotmail.com>
2020-07-12 14:01:41 +0800

    8皇后排列

---
+++ Algorithm/剑指Offer/4.4 8皇后排列.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.剑指Offer {
+    public class QueenCombination {
+        static int count = 0;
+
+        void Test() {
+            int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7 };
+            Perform(arr, 0);
+            count = 0;
+            System.Console.WriteLine(count);
+        }
+
+        public static void Perform(int[] arr, int start) {
+            if (start == arr.Length) {
+                if (Check(arr)) {
+                    count++;
+                }
+                return;
+            }
+            for (int i = start; i < arr.Length; i++) {
+                int tmp = arr[start];
+                arr[start] = arr[i];
+                arr[i] = tmp;
+
+                Perform(arr, start + 1);
+
+                tmp = arr[start];
+                arr[start] = arr[i];
+                arr[i] = tmp;
+            }
+        }
+
+        /// <summary>
+        /// 检查是否有两个皇后在对角线上
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns>有返回false</returns>
+        static bool Check(int[] arr) {
+            for (int i = 0; i < arr.Length; i++) {
+                for (int j = i + 1; j < arr.Length; j++) {
+                    if (i - j == arr[i] - arr[j] || j - i == arr[i] - arr[j]) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Algorithm/剑指Offer/4.4 字符的所有组合.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.剑指Offer {
+    public class CharsCombination {
+
+        public static void Test() {
+            char[] str = "abcd".ToCharArray();
+            CharsCombination.Perform(str);
+        }
+
+        public static void Perform(char[] str) {
+            for (int m = 1; m <= str.Length; m++) {
+                Stack<char> stack = new Stack<char>();
+                Combination(str, 0, m, stack);
+            }
+        }
+
+        static void Combination(char[] str, int start, int m, Stack<char> stack) {
+            if (m == 0) {
+                PrintStack(stack);
+                return;
+            }
+            int n = str.Length;
+            if (start == str.Length) {
+                return;
+            }
+            char first = str[start];
+            stack.Push(first);
+            Combination(str, start + 1, m - 1, stack);
+            stack.Pop();
+            Combination(str, start + 1, m, stack);
+        }
+
+        static void PrintStack(Stack<char> stack) {
+            if (stack.Count > 0) {
+                Stack<char> stack2 = new Stack<char>();
+                while (stack.Count > 0) {
+                    char c = stack.Pop();
+                    stack2.Push(c);
+                }
+                while (stack2.Count > 0) {
+                    char c = stack2.Pop();
+                    System.Console.Write(c);
+                    stack.Push(c);
+                }
+                System.Console.WriteLine();
+            }
+        }
+    }
+}
