14d1cb606ac797602a4775eff58d96b2054156f7
WHB <wanghongboskate@hotmail.com>
2020-07-29 22:46:11 +0800

    最长无重复子串

---
+++ Algorithm/剑指Offer/45.把数字翻译成字符串.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Algorithm.剑指Offer {
+    public class GetTranslationCount {
+
+        void Test() {
+            int num = 262;
+            int count = Perform(num);
+        }
+
+        public static int Perform(int num) {
+            string str = num.ToString();
+            int[] aux = new int[str.Length + 1];
+            for (int i = str.Length; i >= 0; i--) {
+                if (i == str.Length || i == str.Length - 1) {
+                    aux[i] = 1;
+                } else {
+                    aux[i] = aux[i + 1] + aux[i + 2] * Check(str, i, i + 1);
+                }
+            }
+            return aux[0];
+        }
+
+        static int Check(string str, int i, int j) {
+            int x = str[i] - '0';
+            int y = str[j] - '0';
+            int xy = x * 10 + y;
+            return xy > 25 ? 0 : 1;
+        }
+    }
+}
---
+++ Algorithm/剑指Offer/48.最长不含重复字符的子字符串.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithm.剑指Offer {
+    public class LongestSubStringWithoutDuplication {
+
+        static void Test() {
+            string str = "arabcacfrx";
+            int count = Perform(str);
+        }
+
+        public static int Perform(string str) {
+            int curLength = 0;
+            int maxLength = 0;
+            Dictionary<char, int> positions = new Dictionary<char, int>();
+
+            for (int i = 0; i < str.Length; i++) {
+                char c = str[i];
+                if (!positions.ContainsKey(str[i]) || i - positions[str[i]] > curLength) {
+                    curLength += 1;
+                } else {
+                    curLength = i - positions[str[i]];
+                }
+                positions[str[i]] = i;
+                if (curLength > maxLength) {
+                    maxLength = curLength;
+                }
+            }
+
+            return maxLength;
+        }
+    }
+}
