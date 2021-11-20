dd5f64a06e7a05ae8599b454568cf2f6351aff07
wanghongbo <wanghongboskate@hotmail.com>
2021-11-20 08:57:16 +0800

    升级Visual Studio 2022

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1,8 +1,15 @@
-﻿
-namespace Algorithm {
-    class Program {
-        static void Main(string[] args) {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            
         }
     }
 }
